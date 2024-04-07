fixing calculation errors (float64)

draw_functions.py
@@ -16,18 +16,15 @@ def draw_func(population):
     F = f(x1, x2)
     # plt.contour(X1, X2, f(X1, X2))
 
-    E = 45
-    A = 45
-
     fig = plt.figure(figsize=[12, 8])
     ax = plt.axes(projection='3d')
     ax.plot_surface(X1, X2, f(X1, X2), cmap='jet', alpha=0.5)
     # ax.plot_wireframe(X1, X2, f(X1, X2), rcount=15, ccount=15)
-    # ax.view_init(elev=E, azim=A)
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('f(X, Y)')
     # ax.contourf(x1, x2, f(X1, X2))
+    ax.view_init(70, -60)
     for el in population:
         ax.scatter3D(el.vector[0], el.vector[1], el.evaluation_value, color="red")
     plt.show()
@@ -87,6 +84,7 @@ def draw_styblinski_tang(population):
     x, y = np.meshgrid(x, y)
     z = 0.5 * ((x ** 4 + y ** 4) - 16 * (x ** 2 + y ** 2) + 5 * (x + y))
     surface = ax.plot_surface(x, y, z, cmap='gist_earth', alpha=0.5)
+    ax.view_init(50, -80)
     for el in population:
         ax.scatter3D(el.vector[0], el.vector[1], el.evaluation_value, color="red")
     plt.show()

some_class.py
@@ -37,7 +37,7 @@ class SomeClass:
         for i in range(self.pop_size):
             temp = []
             for j in range(self.dimensions):
-                temp.append(rn.uniform(self.min_val, self.max_val))
+                temp.append(np.float64(rn.uniform(self.min_val, self.max_val)))
 
             population.append(ind.Individual(np.array(temp), self.function(temp)))
 
@@ -45,8 +45,8 @@ class SomeClass:
 
     def mutate(self, i1, i2, i3):
         temp_individual_vector = i1.vector + (self.f * (i2.vector - i3.vector))
-        print('i1: {}+, f: {}* i2:{}-, i3:{} = {}'.format(i1.vector, self.f, i2.vector, i3.vector,
-                                                          temp_individual_vector))
+        # print('i1: {}+, f: {}* i2:{}-, i3:{} = {}'.format(i1.vector, self.f, i2.vector, i3.vector,
+        # temp_individual_vector))
         temp_individual = ind.Individual(temp_individual_vector, self.function(temp_individual_vector))
         return temp_individual
 
@@ -81,12 +81,12 @@ class SomeClass:
         while cur_iter < self.iterations:
             test = []
             for individual in self.population:
-                individuals = rn.choices(self.population, k=3)
-                temp_ind = self.mutate(individuals[0], individuals[1], individuals[2])
+                individuals = rn.choices(self.population, k=2)
+                temp_ind = self.mutate(individual, individuals[0], individuals[1])
                 crossbreed_ind = self.crossbreed(individual, temp_ind)
                 test.append(self.selection(individual, crossbreed_ind, EvaluationType.MINIMUM))
             cur_iter += 1
-            self.population = test #copy.deepcopy(test)
+            self.population = test  # copy.deepcopy(test)
 
             if cur_iter == self.iterations / 2:
                 self.f = self.f / 2
@@ -95,9 +95,8 @@ class SomeClass:
             for el in self.population:
                 print(el)
             # DEBUG
-            if cur_iter % 10 == 0:
-                df.draw_rastrigin(self.population)
-
+            if cur_iter % 100 == 0:
+                df.draw_func(self.population)
 
 
 class EvaluationType(Enum):